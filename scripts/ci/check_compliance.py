8287c7ad61a26442f5d2dbe007e75558113647b3
Pieter De Gendt <pieter.degendt@basalte.be>
2025-06-18 10:08:19 +0200

    scripts: ci: check_compliance: Pass path representation to magic
    On windows the Magic.from_file method fails to convert a PathLike to its
    representation.
    Pass using os.fspath.
    
    Signed-off-by: Pieter De Gendt <pieter.degendt@basalte.be>

--- scripts/ci/check_compliance.py
+++ scripts/ci/check_compliance.py
@@ -1545,7 +1545,7 @@ class ImageSize(ComplianceTest):
 
         for file in get_files(filter="d"):
             full_path = GIT_TOP / file
-            mime_type = magic.from_file(full_path, mime=True)
+            mime_type = magic.from_file(os.fspath(full_path), mime=True)
 
             if not mime_type.startswith("image/"):
                 continue
@@ -1733,7 +1733,7 @@ class KeepSorted(ComplianceTest):
         return -1
 
     def check_file(self, file, fp):
-        mime_type = magic.from_file(file, mime=True)
+        mime_type = magic.from_file(os.fspath(file), mime=True)
 
         if not mime_type.startswith("text/"):
             return
@@ -1905,7 +1905,7 @@ class TextEncoding(ComplianceTest):
 
         for file in get_files(filter="d"):
             full_path = GIT_TOP / file
-            mime_type = m.from_file(full_path)
+            mime_type = m.from_file(os.fspath(full_path))
 
             if not mime_type.startswith("text/"):
                 continue
