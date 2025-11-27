zephyrproject-rtos/zephyr

arch/x86: Shadow Stack support
Shadow Stack is one of the capabilities provided by Intel Control-flow
Enforcement Technology (CET), aimed at defending against Return Oriented
Programming.

This patch enables it for x86_64 (32-bit support coming in future
patches):

    - Add relevant Kconfigs;
    - Shadow stacks should live in specially defined memory pages, so
      gen_mmu.py was updated to allow that;
    - A new macro, Z_X86_SHADOW_STACK_DEFINE, added to define the area
      for a shadow stack;
    - A new function, z_x86_thread_attach_shadow_stack(), added to
      attach a shadow stack to a never started thread;
    - locore.S changed to enable/disable shadow stack when a thread
      using it comes in/out of execution.

As not all threads are currently shadow stack capable, threads that do
not use it will still run with shadow stack disabled. Ideally, at some
point in the future, all threads would use the shadow stack, so no need
to disable it at all.

Signed-off-by: Ederson de Souza <ederson.desouza@intel.com>

arch/x86/core/fatal.c
@@ -265,6 +265,14 @@ static void dump_regs(const struct arch_esf *esf)
 		esf->rsp, esf->rflags, esf->cs & 0xFFFFU, get_cr3(esf));
 
 	LOG_ERR("RIP: 0x%016lx", esf->rip);
+#ifdef CONFIG_HW_SHADOW_STACK
+	{
+	uintptr_t ssp;
+
+		__asm__ volatile("rdsspq %0" : "=r"(ssp));
+		LOG_ERR("SSP: 0x%016lx", ssp);
+	}
+#endif /* CONFIG_HW_SHADOW_STACK */
 }
 #else /* 32-bit */
 __pinned_func

InventoryTech/cfa-nrf-sdk-zephyr

arch/x86/core/fatal.c
@@ -265,6 +265,14 @@ static void dump_regs(const struct arch_esf *esf)
 		esf->rsp, esf->rflags, esf->cs & 0xFFFFU, get_cr3(esf));
 
 	EXCEPTION_DUMP("RIP: 0x%016lx", esf->rip);
+#ifdef CONFIG_HW_SHADOW_STACK
+	{
+	uintptr_t ssp;
+
+		__asm__ volatile("rdsspq %0" : "=r"(ssp));
+		EXCEPTION_DUMP("SSP: 0x%016lx", ssp);
+	}
+#endif /* CONFIG_HW_SHADOW_STACK */
 }
 #else /* 32-bit */
 __pinned_func