zephyrproject-rtos/zephyr

dt-bindings: stm32mp2_clock.h: add clock for i2c peripherals
Add clock bindings for I2C peripherals for the STM32MP2 series.

Signed-off-by: Tanguy Raufflet <tanguy.raufflet@savoirfairelinux.com>

include/zephyr/dt-bindings/clock/stm32mp2_clock.h
@@ -50,7 +50,16 @@
 #define STM32_CLOCK_PERIPH_UART8	0x798
 #define STM32_CLOCK_PERIPH_UART9	0x79C
 
+/* I2C Peripheral */
+#define STM32_CLOCK_PERIPH_I2C1		0x7A0
+#define STM32_CLOCK_PERIPH_I2C2		0x7A8
+#define STM32_CLOCK_PERIPH_I2C3		0x7AC
+#define STM32_CLOCK_PERIPH_I2C4		0x7B0
+#define STM32_CLOCK_PERIPH_I2C5		0x7B4
+#define STM32_CLOCK_PERIPH_I2C6		0x7B8
+#define STM32_CLOCK_PERIPH_I2C7		0x7BC
+
 #define STM32_CLOCK_PERIPH_MIN	STM32_CLOCK_PERIPH_GPIOA
-#define STM32_CLOCK_PERIPH_MAX	STM32_CLOCK_PERIPH_UART9
+#define STM32_CLOCK_PERIPH_MAX	STM32_CLOCK_PERIPH_I2C7
 
 #endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_STM32MP2_CLOCK_H_ */

InventoryTech/cfa-nrf-sdk-zephyr

include/zephyr/dt-bindings/clock/stm32mp2_clock.h
@@ -50,7 +50,17 @@
 #define STM32_CLOCK_PERIPH_UART8	0x798
 #define STM32_CLOCK_PERIPH_UART9	0x79C
 
+/* I2C Peripheral */
+#define STM32_CLOCK_PERIPH_I2C1		0x7A0
+#define STM32_CLOCK_PERIPH_I2C2		0x7A8
+#define STM32_CLOCK_PERIPH_I2C3		0x7AC
+#define STM32_CLOCK_PERIPH_I2C4		0x7B0
+#define STM32_CLOCK_PERIPH_I2C5		0x7B4
+#define STM32_CLOCK_PERIPH_I2C6		0x7B8
+#define STM32_CLOCK_PERIPH_I2C7		0x7BC
+#define STM32_CLOCK_PERIPH_I2C8		0x7C0
+
 #define STM32_CLOCK_PERIPH_MIN	STM32_CLOCK_PERIPH_GPIOA
-#define STM32_CLOCK_PERIPH_MAX	STM32_CLOCK_PERIPH_UART9
+#define STM32_CLOCK_PERIPH_MAX	STM32_CLOCK_PERIPH_I2C8
 
 #endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_STM32MP2_CLOCK_H_ */