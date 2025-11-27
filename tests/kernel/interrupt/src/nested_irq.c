zephyrproject-rtos/zephyr

tests/kernel: Avoid C library calls from interrupt handlers
On arm64, GCC feels free to use floating point registers for essentially
anything unless we build with -mgeneral-regs-only. After the FPU gets used
in an interrupt handler, interrupts are disabled as there's no place to
save the registers during a nested exception. As the C library may be built
separately without this flag, we cannot use any of its functions or risk
having nested exceptions fail.

Switch printk usage to k_str_out and stick to (mostly) Zephyr internal
functions to ensure nested interrupts work correctly.

Signed-off-by: Keith Packard <keithp@keithp.com>

tests/kernel/interrupt/src/nested_irq.c
@@ -102,19 +102,19 @@ void isr1(const void *param)
 {
 	ARG_UNUSED(param);
 
-	printk("isr1: Enter\n");
+	k_str_out_count("isr1: Enter\n");
 
 	/* Set verification token */
 	isr1_result = ISR1_TOKEN;
 
-	printk("isr1: Leave\n");
+	k_str_out_count("isr1: Leave\n");
 }
 
 void isr0(const void *param)
 {
 	ARG_UNUSED(param);
 
-	printk("isr0: Enter\n");
+	k_str_out_count("isr0: Enter\n");
 
 	/* Set verification token */
 	isr0_result = ISR0_TOKEN;
@@ -128,7 +128,7 @@ void isr0(const void *param)
 	/* Validate nested ISR result token */
 	zassert_equal(isr1_result, ISR1_TOKEN, "isr1 did not execute");
 
-	printk("isr0: Leave\n");
+	k_str_out_count("isr0: Leave\n");
 }
 
 /**

InventoryTech/cfa-nrf-sdk-zephyr

tests/kernel/interrupt/src/nested_irq.c
@@ -102,19 +102,19 @@ void isr1(const void *param)
 {
 	ARG_UNUSED(param);
 
-	printk("isr1: Enter\n");
+	k_str_out_count("ISR1: Enter\n");
 
 	/* Set verification token */
 	isr1_result = ISR1_TOKEN;
 
-	printk("isr1: Leave\n");
+	k_str_out_count("ISR1: Leave\n");
 }
 
 void isr0(const void *param)
 {
 	ARG_UNUSED(param);
 
-	printk("isr0: Enter\n");
+	k_str_out_count("ISR0: Enter\n");
 
 	/* Set verification token */
 	isr0_result = ISR0_TOKEN;
@@ -128,7 +128,7 @@ void isr0(const void *param)
 	/* Validate nested ISR result token */
 	zassert_equal(isr1_result, ISR1_TOKEN, "isr1 did not execute");
 
-	printk("isr0: Leave\n");
+	k_str_out_count("ISR0: Leave\n");
 }
 
 /**