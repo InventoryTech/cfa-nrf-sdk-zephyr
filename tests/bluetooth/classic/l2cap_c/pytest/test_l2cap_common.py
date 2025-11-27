zephyrproject-rtos/zephyr

tests: bluetooth: classic: Add test suite l2cap client.
IUT works as a l2cap client with basic mode. The peer device,
l2cap server with basic mode, is a PC with running bumble on it.
This test only performs the function of L2CAP basic mode.
Support multiple l2cap enerties in new shell l2cap_br,
which may support more test function.

In the test suite , there are two groups in test cases.
Group 1 Including case1-case24 focuses on connection and
disconnection around l2cap. The impact of active and passive
acl connectivity, disconnectivity and authentication as well as
disconnection from ACL without l2cap disconnect is tested.

Group2 Including case25-case31 revolves around the basic
 parameters of L2CAPserver configuration, data transfer.
Case 25: Test l2cap connection with max MTU(0xffff).But the max mtu
which the stack supports is (CONFIG_BT_BUF_ACL_RX_SIZE - 4U = 196).
Case 26:  Test l2cap multi_channel connection and data tranfer.
Case 27: Test l2cap connection with invaild PSM.
Case 28: Test l2cap re_connect after disconnected.
Case 29: Test l2cap connection with min MTU(0x30).set server mtu = 48,
client mtu=48. l2cap connect successfully,
both server and client mtu should be 48.
Case 30: Test l2cap connection with min MTU(0x30). set server mtu = 48,
client mtu=0x27. l2cap connect fail.
Case 31: Test l2cap connection with min MTU(0x30).set server mtu = 30,
client mtu=48.l2cap connect successfully,
both server and client mtu should be 48.

test_l2cap_client.py is a file containing real test cases.
test_l2cap_common.py is a common file. It encapsulates some
test function functions that are commonly used for test cases.

Signed-off-by: Cheng Chang <cheng.chang@nxp.com>

tests/bluetooth/classic/l2cap_c/pytest/test_l2cap_common.py
@@ -1,0 +1,108 @@
+# Copyright 2025 NXP
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import asyncio
+import logging
+
+from bumble.core import BT_BR_EDR_TRANSPORT
+from bumble.hci import (
+    HCI_CONNECTION_REJECTED_DUE_TO_LIMITED_RESOURCES_ERROR,
+)
+from bumble.pairing import PairingDelegate
+
+logger = logging.getLogger(__name__)
+
+L2CAP_SERVER_PSM = 0x1001
+MODE = "basic"
+L2CAP_CHAN_IUT_ID = 0
+MAX_MTU = 0xFFFF
+MIN_MTU = 0x30
+STRESS_TEST_MAX_COUNT = 50
+
+
+class Delegate(PairingDelegate):
+    def __init__(
+        self,
+        dut,
+        io_capability,
+    ):
+        super().__init__(
+            io_capability,
+        )
+        self.dut = dut
+
+
+async def device_power_on(device) -> None:
+    while True:
+        try:
+            await device.power_on()
+            break
+        except Exception:
+            continue
+
+
+async def wait_for_shell_response(dut, message, timeout=5):
+    found = False
+    lines = []
+    try:
+        for _ in range(0, timeout):
+            if found is False:
+                read_lines = dut.readlines()
+                for line in read_lines:
+                    if message in line:
+                        found = True
+                        break
+                lines = lines + read_lines
+                await asyncio.sleep(1)
+    except Exception as e:
+        logger.error(f'{e}!', exc_info=True)
+        raise e
+    return found, lines
+
+
+async def send_cmd_to_iut(shell, dut, cmd, parse=None, timeout=10):
+    found = False
+    lines = shell.exec_command(cmd)
+    if parse is not None:
+        found = any([parse in line for line in lines])
+        if found is False:
+            found, lines = await wait_for_shell_response(dut, parse, timeout)
+    else:
+        found = True
+    logger.info(f'{lines}')
+    assert found is True
+    return lines
+
+
+async def bumble_acl_connect(shell, dut, device, target_address):
+    connection = None
+    try:
+        connection = await device.connect(target_address, transport=BT_BR_EDR_TRANSPORT)
+        logger.info(f'=== Connected to {connection.peer_address}!')
+    except Exception as e:
+        logger.error(f'Fail to connect to {target_address}!')
+        raise e
+    return connection
+
+
+async def bumble_acl_disconnect(shell, dut, device, connection):
+    await device.disconnect(
+        connection, reason=HCI_CONNECTION_REJECTED_DUE_TO_LIMITED_RESOURCES_ERROR
+    )
+    found, lines = await wait_for_shell_response(dut, "Disconnected:")
+    logger.info(f'lines : {lines}')
+    assert found is True
+    return found, lines
+
+
+async def bumble_l2cap_disconnect(shell, dut, incoming_channel, iut_id=L2CAP_CHAN_IUT_ID):
+    try:
+        await incoming_channel.disconnect()
+    except Exception as e:
+        logger.error('Fail to send l2cap disconnect command!')
+        raise e
+    assert incoming_channel.disconnection_result is None
+
+    found, _ = await wait_for_shell_response(dut, f"Channel {iut_id} disconnected")
+    assert found is True

InventoryTech/cfa-nrf-sdk-zephyr

tests/bluetooth/classic/l2cap_c/pytest/test_l2cap_common.py
@@ -1,0 +1,109 @@
+# Copyright 2025 NXP
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import asyncio
+import logging
+
+from bumble.core import BT_BR_EDR_TRANSPORT
+from bumble.hci import (
+    HCI_CONNECTION_REJECTED_DUE_TO_LIMITED_RESOURCES_ERROR,
+)
+from bumble.pairing import PairingDelegate
+
+logger = logging.getLogger(__name__)
+
+L2CAP_SERVER_PSM = 0x1001
+L2CAP_CHAN_IUT_ID = 0
+MAX_MTU = 0xFFFF
+MIN_MTU = 0x30
+STRESS_TEST_MAX_COUNT = 50
+
+
+class Delegate(PairingDelegate):
+    def __init__(
+        self,
+        dut,
+        io_capability,
+    ):
+        super().__init__(
+            io_capability,
+        )
+        self.dut = dut
+
+
+async def device_power_on(device) -> None:
+    while True:
+        try:
+            await device.power_on()
+            break
+        except Exception:
+            continue
+
+
+async def wait_for_shell_response(dut, message, max_time=5):
+    found = False
+    lines = []
+    try:
+        for _ in range(0, max_time):
+            if found is False:
+                read_lines = dut.readlines()
+                for line in read_lines:
+                    if message in line:
+                        found = True
+                        break
+                lines = lines + read_lines
+                await asyncio.sleep(1)
+    except Exception as e:
+        logger.error(f'{e}!', exc_info=True)
+        raise e
+    return found, lines
+
+
+async def send_cmd_to_iut(shell, dut, cmd, parse=None, max_time=10):
+    found = False
+    lines = shell.exec_command(cmd)
+    if parse is not None:
+        for line in lines:
+            if parse in line:
+                found = True
+        if found is False:
+            found, lines = await wait_for_shell_response(dut, parse, max_time)
+    else:
+        found = True
+    logger.info(f'{lines}')
+    assert found is True
+    return lines
+
+
+async def bumble_acl_connect(shell, dut, device, target_address):
+    connection = None
+    try:
+        connection = await device.connect(target_address, transport=BT_BR_EDR_TRANSPORT)
+        logger.info(f'=== Connected to {connection.peer_address}!')
+    except Exception as e:
+        logger.error(f'Fail to connect to {target_address}!')
+        raise e
+    return connection
+
+
+async def bumble_acl_disconnect(shell, dut, device, connection):
+    await device.disconnect(
+        connection, reason=HCI_CONNECTION_REJECTED_DUE_TO_LIMITED_RESOURCES_ERROR
+    )
+    found, lines = await wait_for_shell_response(dut, "Disconnected:")
+    logger.info(f'lines : {lines}')
+    assert found is True
+    return found, lines
+
+
+async def bumble_l2cap_disconnect(shell, dut, incoming_channel, iut_id=L2CAP_CHAN_IUT_ID):
+    try:
+        await incoming_channel.disconnect()
+    except Exception as e:
+        logger.error('Fail to send l2cap disconnect command!')
+        raise e
+    assert incoming_channel.disconnection_result is None
+
+    found, _ = await wait_for_shell_response(dut, f"Channel {iut_id} disconnected")
+    assert found is True