zephyrproject-rtos/zephyr

soc: nordic: nrf54h: transition from gpd to zephyr pinctrl and pds
Transition nrf54h away from the soc specific gpd
(global power domain) driver which mixed power domains, pinctrl
and gpio pin retention into a non scalable solution, forcing soc
specific logic to bleed into nrf drivers.

The new solution uses zephyrs PM_DEVICE based power domains to
properly model the hardware layout of device and pin power domains,
and moves pin retention logic out of drivers into pinctrl and
gpio, which are the components which manage pins (pads).

Signed-off-by: Bjarki Arge Andreasen <bjarki.andreasen@nordicsemi.no>

drivers/counter/counter_nrfx_timer.c
@@ -35,22 +35,8 @@ LOG_MODULE_REGISTER(LOG_MODULE_NAME, LOG_LEVEL);
 #define MAYBE_CONST_CONFIG const
 #endif
 
-#ifdef CONFIG_SOC_NRF54H20_GPD
-#include <nrf/gpd.h>
-
-#define NRF_CLOCKS_INSTANCE_IS_FAST(node)						\
-	COND_CODE_1(DT_NODE_HAS_PROP(node, power_domains),				\
-		    (IS_EQ(DT_PHA(node, power_domains, id), NRF_GPD_FAST_ACTIVE1)),	\
-		    (0))
-
-/* Macro must resolve to literal 0 or 1 */
-#define INSTANCE_IS_FAST(idx) NRF_CLOCKS_INSTANCE_IS_FAST(DT_DRV_INST(idx))
-
-#define INSTANCE_IS_FAST_OR(idx) INSTANCE_IS_FAST(idx) ||
-
-#if (DT_INST_FOREACH_STATUS_OKAY(INSTANCE_IS_FAST_OR) 0)
+#if NRF_DT_INST_ANY_IS_FAST
 #define COUNTER_ANY_FAST 1
-#endif
 #endif
 
 struct counter_nrfx_data {
@@ -474,13 +460,13 @@ static DEVICE_API(counter, counter_nrfx_driver_api) = {
  * which is using nrfs (IPC) are initialized later.
  */
 #define TIMER_INIT_LEVEL(idx) \
-	COND_CODE_1(INSTANCE_IS_FAST(idx), (POST_KERNEL), (PRE_KERNEL_1))
+	COND_CODE_1(NRF_DT_IS_FAST(idx), (POST_KERNEL), (PRE_KERNEL_1))
 
 /* Get initialization priority of an instance. Instances that requires clock control
  * which is using nrfs (IPC) are initialized later.
  */
 #define TIMER_INIT_PRIO(idx)								\
-	COND_CODE_1(INSTANCE_IS_FAST(idx),						\
+	COND_CODE_1(NRF_DT_IS_FAST(idx),						\
 		    (UTIL_INC(CONFIG_CLOCK_CONTROL_NRF_HSFLL_GLOBAL_INIT_PRIORITY)),	\
 		    (CONFIG_COUNTER_INIT_PRIORITY))
 
@@ -536,7 +522,7 @@ static DEVICE_API(counter, counter_nrfx_driver_api) = {
 		},										\
 		.ch_data = counter##idx##_ch_data,						\
 		.timer = (NRF_TIMER_Type *)DT_INST_REG_ADDR(idx),				\
-		IF_ENABLED(INSTANCE_IS_FAST(idx),						\
+		IF_ENABLED(NRF_DT_IS_FAST(idx),							\
 			(.clk_dev = DEVICE_DT_GET(DT_CLOCKS_CTLR(DT_DRV_INST(idx))),		\
 			 .clk_spec = {								\
 				.frequency = NRF_PERIPH_GET_FREQUENCY(DT_DRV_INST(idx)),	\

InventoryTech/cfa-nrf-sdk-zephyr

drivers/counter/counter_nrfx_timer.c
@@ -35,11 +35,8 @@ LOG_MODULE_REGISTER(LOG_MODULE_NAME, LOG_LEVEL);
 #define MAYBE_CONST_CONFIG const
 #endif
 
-#if NRF_DT_INST_ANY_IS_FAST && CONFIG_CLOCK_CONTROL
-#define COUNTER_IS_FAST(idx) NRF_DT_INST_IS_FAST(idx)
-#define COUNTER_ANY_FAST
-#else
-#define COUNTER_IS_FAST(idx) 0
+#if NRF_DT_INST_ANY_IS_FAST
+#define COUNTER_ANY_FAST 1
 #endif
 
 struct counter_nrfx_data {
@@ -463,13 +460,13 @@ static DEVICE_API(counter, counter_nrfx_driver_api) = {
  * which is using nrfs (IPC) are initialized later.
  */
 #define TIMER_INIT_LEVEL(idx) \
-	COND_CODE_1(COUNTER_IS_FAST(idx), (POST_KERNEL), (PRE_KERNEL_1))
+	COND_CODE_1(NRF_DT_INST_IS_FAST(idx), (POST_KERNEL), (PRE_KERNEL_1))
 
 /* Get initialization priority of an instance. Instances that requires clock control
  * which is using nrfs (IPC) are initialized later.
  */
 #define TIMER_INIT_PRIO(idx)								\
-	COND_CODE_1(INSTANCE_IS_FAST(idx),						\
+	COND_CODE_1(NRF_DT_INST_IS_FAST(idx),						\
 		    (UTIL_INC(CONFIG_CLOCK_CONTROL_NRF_HSFLL_GLOBAL_INIT_PRIORITY)),	\
 		    (CONFIG_COUNTER_INIT_PRIORITY))
 
@@ -525,8 +522,8 @@ static DEVICE_API(counter, counter_nrfx_driver_api) = {
 		},										\
 		.ch_data = counter##idx##_ch_data,						\
 		.timer = (NRF_TIMER_Type *)DT_INST_REG_ADDR(idx),				\
-		IF_ENABLED(COUNTER_IS_FAST(idx),						\
-			(.clk_dev = DEVICE_DT_GET_OR_NULL(DT_CLOCKS_CTLR(DT_DRV_INST(idx))),	\
+		IF_ENABLED(NRF_DT_INST_IS_FAST(idx),						\
+			(.clk_dev = DEVICE_DT_GET(DT_CLOCKS_CTLR(DT_DRV_INST(idx))),		\
 			 .clk_spec = {								\
 				.frequency = NRF_PERIPH_GET_FREQUENCY(DT_DRV_INST(idx)),	\
 				.accuracy = 0,							\