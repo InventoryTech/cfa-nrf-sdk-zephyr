zephyrproject-rtos/zephyr

Bluetooth: tester: Support feature ISO broadcaster
Add command `BTP_GAP_CREATE_BIG` to create BIG.

Add command `BTP_GAP_BIS_BROADCAST` to send broadcast data.

Signed-off-by: Lyle Zhu <lyle.zhu@nxp.com>

tests/bluetooth/tester/src/btp/btp_gap.h
@@ -362,6 +362,30 @@ struct btp_gap_big_create_sync_cmd {
 	uint8_t broadcast_code[0];
 } __packed;
 
+#define BTP_GAP_CREATE_BIG_ENC_DISABLE		0x00
+#define BTP_GAP_CREATE_BIG_ENC_ENABLE		0x01
+
+#define BTP_GAP_CREATE_BIG			0x2c
+struct btp_gap_create_big_cmd {
+	uint8_t id;
+	uint8_t num_bis;
+	uint32_t interval;
+	uint16_t latency;
+	uint8_t rtn;
+	uint8_t phy;
+	uint8_t packing;
+	uint8_t framing;
+	uint8_t encryption;
+	uint8_t broadcast_code[0];
+} __packed;
+
+#define BTP_GAP_BIS_BROADCAST			0x2d
+struct btp_gap_bis_broadcast_cmd {
+	uint8_t bis_id;
+	uint8_t data_len;
+	uint8_t data[];
+} __packed;
+
 #define BTP_GAP_SET_RPA_TIMEOUT                 0x30
 struct btp_gap_set_rpa_timeout_cmd {
 	uint16_t rpa_timeout;

InventoryTech/cfa-nrf-sdk-zephyr

tests/bluetooth/tester/src/btp/btp_gap.h
@@ -363,6 +363,30 @@ struct btp_gap_big_create_sync_cmd {
 	uint8_t broadcast_code[0];
 } __packed;
 
+#define BTP_GAP_CREATE_BIG_ENC_DISABLE		0x00
+#define BTP_GAP_CREATE_BIG_ENC_ENABLE		0x01
+
+#define BTP_GAP_CREATE_BIG			0x2d
+struct btp_gap_create_big_cmd {
+	uint8_t id;
+	uint8_t num_bis;
+	uint32_t interval;
+	uint16_t latency;
+	uint8_t rtn;
+	uint8_t phy;
+	uint8_t packing;
+	uint8_t framing;
+	uint8_t encryption;
+	uint8_t broadcast_code[0];
+} __packed;
+
+#define BTP_GAP_BIS_BROADCAST			0x2e
+struct btp_gap_bis_broadcast_cmd {
+	uint8_t bis_id;
+	uint8_t data_len;
+	uint8_t data[];
+} __packed;
+
 #define BTP_GAP_SET_RPA_TIMEOUT                 0x30
 struct btp_gap_set_rpa_timeout_cmd {
 	uint16_t rpa_timeout;