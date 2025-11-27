zephyrproject-rtos/zephyr

arch/x86: Support shadow stack on ia32
Most notable difference on base support is the need to keep the shadow
stack tokens, which are 8 bytes, 8 bytes aligned. Some helper macros are
used for that.

Also, an `ssp` entry is added to the task state segment (TSS).

Signed-off-by: Ederson de Souza <ederson.desouza@intel.com>

arch/x86/core/fatal.c
@@ -286,6 +286,14 @@ static void dump_regs(const struct arch_esf *esf)
 		esf->cs & 0xFFFFU, get_cr3(esf));
 
 	LOG_ERR("EIP: 0x%08x", esf->eip);
+#ifdef CONFIG_HW_SHADOW_STACK
+	{
+	uintptr_t ssp;
+
+		__asm__ volatile("rdsspd %0" : "=r"(ssp));
+		LOG_ERR("SSP: 0x%08lx", ssp);
+	}
+#endif /* CONFIG_HW_SHADOW_STACK */
 }
 #endif /* CONFIG_X86_64 */
 

include/zephyr/arch/x86/cet.h
@@ -21,6 +21,7 @@ extern FUNC_NORETURN void z_thread_entry(k_thread_entry_t entry,
 
 typedef long z_x86_shadow_stack_t;
 
+#ifdef CONFIG_X86_64
 #define Z_X86_SHADOW_STACK_DEFINE(name, size) \
 	z_x86_shadow_stack_t Z_GENERIC_SECTION(.x86shadowstack) \
 	__aligned(CONFIG_X86_CET_SHADOW_STACK_ALIGNMENT) \
@@ -33,6 +34,28 @@ typedef long z_x86_shadow_stack_t;
 		   (uintptr_t)z_thread_entry, \
 		  [size / sizeof(z_x86_shadow_stack_t) - 2] = \
 		   (uintptr_t)X86_KERNEL_CS }
+#else
+
+#ifdef CONFIG_DEBUG_INFO
+extern void z_x86_thread_entry_wrapper(k_thread_entry_t entry,
+				      void *p1, void *p2, void *p3);
+#define ___x86_entry_point z_x86_thread_entry_wrapper
+#else
+#define ___x86_entry_point z_thread_entry
+#endif
+
+#define Z_X86_SHADOW_STACK_DEFINE(name, size) \
+	z_x86_shadow_stack_t Z_GENERIC_SECTION(.x86shadowstack) \
+	__aligned(CONFIG_X86_CET_SHADOW_STACK_ALIGNMENT) \
+	name[size / sizeof(z_x86_shadow_stack_t)] = \
+		{ [size / sizeof(z_x86_shadow_stack_t) - 4] = \
+		   (uintptr_t)name + size - 2 * sizeof(z_x86_shadow_stack_t), \
+		  [size / sizeof(z_x86_shadow_stack_t) - 3] = 0, \
+		  [size / sizeof(z_x86_shadow_stack_t) - 2] = \
+		   (uintptr_t)___x86_entry_point, \
+		}
+
+#endif
 
 int z_x86_thread_attach_shadow_stack(k_tid_t thread,
 				     z_x86_shadow_stack_t *stack,

InventoryTech/cfa-nrf-sdk-zephyr

arch/x86/core/fatal.c
@@ -286,6 +286,14 @@ static void dump_regs(const struct arch_esf *esf)
 		esf->cs & 0xFFFFU, get_cr3(esf));
 
 	EXCEPTION_DUMP("EIP: 0x%08x", esf->eip);
+#ifdef CONFIG_HW_SHADOW_STACK
+	{
+	uintptr_t ssp;
+
+		__asm__ volatile("rdsspd %0" : "=r"(ssp));
+		EXCEPTION_DUMP("SSP: 0x%08lx", ssp);
+	}
+#endif /* CONFIG_HW_SHADOW_STACK */
 }
 #endif /* CONFIG_X86_64 */
 

include/zephyr/arch/x86/cet.h
@@ -21,6 +21,7 @@ extern FUNC_NORETURN void z_thread_entry(k_thread_entry_t entry,
 
 typedef long z_x86_shadow_stack_t;
 
+#ifdef CONFIG_X86_64
 #define Z_X86_SHADOW_STACK_DEFINE(name, size) \
 	z_x86_shadow_stack_t Z_GENERIC_SECTION(.x86shadowstack) \
 	__aligned(CONFIG_X86_CET_SHADOW_STACK_ALIGNMENT) \
@@ -33,6 +34,28 @@ typedef long z_x86_shadow_stack_t;
 		   (uintptr_t)z_thread_entry, \
 		  [size / sizeof(z_x86_shadow_stack_t) - 2] = \
 		   (uintptr_t)X86_KERNEL_CS }
+#else
+
+#ifdef CONFIG_X86_DEBUG_INFO
+extern void z_x86_thread_entry_wrapper(k_thread_entry_t entry,
+				      void *p1, void *p2, void *p3);
+#define ___x86_entry_point z_x86_thread_entry_wrapper
+#else
+#define ___x86_entry_point z_thread_entry
+#endif
+
+#define Z_X86_SHADOW_STACK_DEFINE(name, size) \
+	z_x86_shadow_stack_t Z_GENERIC_SECTION(.x86shadowstack) \
+	__aligned(CONFIG_X86_CET_SHADOW_STACK_ALIGNMENT) \
+	name[size / sizeof(z_x86_shadow_stack_t)] = \
+		{ [size / sizeof(z_x86_shadow_stack_t) - 4] = \
+		   (uintptr_t)name + size - 2 * sizeof(z_x86_shadow_stack_t), \
+		  [size / sizeof(z_x86_shadow_stack_t) - 3] = 0, \
+		  [size / sizeof(z_x86_shadow_stack_t) - 2] = \
+		   (uintptr_t)___x86_entry_point, \
+		}
+
+#endif
 
 int z_x86_thread_attach_shadow_stack(k_tid_t thread,
 				     z_x86_shadow_stack_t *stack,