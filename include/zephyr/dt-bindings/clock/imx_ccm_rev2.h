zephyrproject-rtos/zephyr

clock: Add KPP clock support for ccm.
Add KPP clock support for ccm.

Signed-off-by: Qiang Zhang <qiang.zhang_6@nxp.com>

include/zephyr/dt-bindings/clock/imx_ccm_rev2.h
@@ -149,6 +149,9 @@
 #define IMX_CCM_I3C1_CLK               0x2200UL
 #define IMX_CCM_I3C2_CLK               0x2201UL
 
+/* KPP */
+#define IMX_CCM_KPP_CLK               0x2300UL
+
 /* QTMR */
 #define IMX_CCM_QTMR_CLK               0x6000UL
 #define IMX_CCM_QTMR1_CLK              0x6000UL

InventoryTech/cfa-nrf-sdk-zephyr

include/zephyr/dt-bindings/clock/imx_ccm_rev2.h
@@ -155,6 +155,9 @@
 #define IMX_CCM_LPIT2_CLK 0x2301UL
 #define IMX_CCM_LPIT3_CLK 0x2302UL
 
+/* KPP */
+#define IMX_CCM_KPP_CLK                0x2400UL
+
 /* QTMR */
 #define IMX_CCM_QTMR_CLK               0x6000UL
 #define IMX_CCM_QTMR1_CLK              0x6000UL