zephyrproject-rtos/zephyr

bluetooth: host: Combine main and sub mode CS step options
Not all combinations of main mode and sub mode are valid, so to make it
harder to select incorrect parameters, the main mode and sub mode
enums are combined into one.

Now the user chooses any valid option in enum bt_conn_le_cs_mode.

Signed-off-by: Olivier Lesage <olivier.lesage@nordicsemi.no>

samples/bluetooth/channel_sounding/src/connected_cs_initiator.c
@@ -309,8 +309,7 @@ int main(void)
 
 	struct bt_le_cs_create_config_params config_params = {
 		.id = CS_CONFIG_ID,
-		.main_mode_type = BT_CONN_LE_CS_MAIN_MODE_2,
-		.sub_mode_type = BT_CONN_LE_CS_SUB_MODE_1,
+		.mode = BT_CONN_LE_CS_MAIN_MODE_2_NO_SUB_MODE,
 		.min_main_mode_steps = 2,
 		.max_main_mode_steps = 10,
 		.main_mode_repetition = 0,

InventoryTech/cfa-nrf-sdk-zephyr

samples/bluetooth/channel_sounding/src/connected_cs_initiator.c
@@ -309,8 +309,7 @@ int main(void)
 
 	struct bt_le_cs_create_config_params config_params = {
 		.id = CS_CONFIG_ID,
-		.main_mode_type = BT_CONN_LE_CS_MAIN_MODE_2,
-		.sub_mode_type = BT_CONN_LE_CS_SUB_MODE_1,
+		.mode = BT_CONN_LE_CS_MAIN_MODE_2_SUB_MODE_1,
 		.min_main_mode_steps = 2,
 		.max_main_mode_steps = 10,
 		.main_mode_repetition = 0,